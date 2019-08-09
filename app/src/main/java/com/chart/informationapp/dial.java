859f7d03ec38ef67716c4739fa778e8991312719
s4daf <sadaf.farhod@gmail.com>
2019-08-09 14:00:35 +0430

    rundial

--- app/src/main/java/com/chart/informationapp/dial.java
+++ app/src/main/java/com/chart/informationapp/dial.java
@@ -2,22 +2,40 @@ package com.chart.informationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.security.Permission;
 
 public class dial extends AppCompatActivity {
+
+    private EditText editText_num;
+    private ImageButton imageButton_dialer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
-        ImageButton btnShow =findViewById(R.id.BtnCall);
-        btnShow.setOnClickListener(new View.OnClickListener() {
+        editText_num =(EditText) findViewById(R.id.EdtNumber);
+        imageButton_dialer  = (ImageButton) findViewById(R.id.BtnCall);
+        imageButton_dialer . setOnClickListener(new View.OnClickListener() {
             @Override
-
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData (Uri.parse("tel:" + editText_num.getText().toString() ));
+                startActivity(intent);
             }
+        });
 
 
-
+    }
 }
